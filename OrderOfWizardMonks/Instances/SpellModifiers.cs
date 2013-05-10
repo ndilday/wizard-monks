9cf50b4664a67a01c3a9bbba6da8679944f28190
ndilday <ndilday@gmail.com>
2013-05-10 18:31:48 -0400

    - skeleton for spells

---
+++ OrderOfWizardMonks/Instances/SpellModifiers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WizardMonks.Instances
+{
+    public static class SpellModifiers
+    {
+        public static int GetModifier(Range range)
+        {
+            switch (range)
+            {
+                case Range.Personal:
+                    return 0;
+                case Range.Touch:
+                case Range.Eye:
+                    return 1;
+                case Range.Voice:
+                    return 2;
+                case Range.Sight:
+                    return 3;
+                case Range.Arcane:
+                    return 4;
+                default:
+                    return 0;
+            }
+        }
+
+        public static int GetModifier(Duration duration)
+        {
+            switch (duration)
+            {
+                case Duration.Instantaneous:
+                    return 0;
+                case Duration.Concentration:
+                case Duration.Diameter:
+                    return 1;
+                case Duration.Sun:
+                case Duration.Circle:
+                    return 2;
+                case Duration.Moon:
+                    return 3;
+                case Duration.Year:
+                    return 4;
+                default:
+                    return 0;
+
+            }
+        }
+
+        public static int GetModifier(Target target)
+        {
+            switch (target)
+            {
+                case Target.Individual:
+                    return 0;
+                case Target.Part:
+                    return 1;
+                case Target.Group:
+                case Target.Ring:
+                    return 2;
+                case Target.Room:
+                    return 3;
+                case Target.Structure:
+                    return 4;
+                case Target.Boundary:
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ OrderOfWizardMonks/Spell.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WizardMonks.Instances;
+
+namespace WizardMonks
+{
+    public enum Range
+    {
+        Personal,
+        Touch,
+        Eye,
+        Voice,
+        Sight,
+        Arcane
+    }
+
+    public enum Duration
+    {
+        Instantaneous,
+        Concentration,
+        Diameter,
+        Sun,
+        Circle,
+        Moon,
+        Year
+    }
+
+    public enum Target
+    {
+        Individual,
+        Part,
+        Group,
+        Ring,
+        Room,
+        Structure,
+        Boundary
+    }
+
+    public class Spell
+    {
+        public ArtPair BaseArts { get; private set; }
+
+        public Range Range { get; private set; }
+        public Duration Duration { get; private set; }
+        public Target Target { get; private set; }
+        public int Base { get; private set; }
+
+        public IEnumerable<Ability> RequisiteTechniques { get; private set; }
+        public IEnumerable<Ability> RequisiteForms { get; private set; }
+        public string Name { get; private set; }
+
+        public double Level
+        {
+            get
+            {
+                int totalModifier = SpellModifiers.GetModifier(Duration) +
+                    SpellModifiers.GetModifier(Range) +
+                    SpellModifiers.GetModifier(Target);
+                if (Base + totalModifier > 5)
+                {
+                    return (Base + totalModifier - 4) * 5;
+                }
+                return Base + totalModifier;
+            }
+        }
+
+        public Spell(ArtPair artPair, Range range, Duration duration, Target target, int baseLevel, string name)
+        {
+            BaseArts = artPair;
+            Range = range;
+            Duration = duration;
+            Target = target;
+            Base = baseLevel;
+            Name = name;
+        }
+    }
+}

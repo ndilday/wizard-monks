33878ade4477e96d5d6616e1bf27995e8d431aa1
Nathan <nadilday@yahoo.com>
2024-02-23 20:18:54 -0500

    - cleaning up logic in LongevityRitualGoal.cs

--- OrderOfWizardMonks/Decisions/Goals/LongevityRitualGoal.cs
+++ OrderOfWizardMonks/Decisions/Goals/LongevityRitualGoal.cs
@@ -7,22 +7,27 @@ namespace WizardMonks.Decisions.Goals
     
     public class LongevityRitualGoal : AGoal
     {
+        // TODO: Make this configurable
+        // TODO: figure out a way to make vis desires split over multiple arts
         private static readonly List<Ability> visTypes = new List<Ability>(){ MagicArts.Creo, MagicArts.Vim };
         public LongevityRitualGoal(Magus magus, uint? ageToCompleteBy, double desire) : base(magus, ageToCompleteBy, desire)
         {
-            this.Conditions.Add(new VisCondition(magus, 140, desire, visTypes, (magus.SeasonalAge / 20) + 1, 1));
+            // target longevity ritual for age 35
+            // assume that it will happen at 35, and therefore require 7 vis
+            this.Conditions.Add(new VisCondition(magus, 140, desire, visTypes, 7, 1));
         }
 
         public override void AddActionPreferencesToList(ConsideredActions alreadyConsidered, IList<string> log)
         {
-            this.Conditions[0] = new VisCondition((Magus)this.Conditions[0].Character, 140, this.Conditions[0].Desire, visTypes, (this.Conditions[0].Character.SeasonalAge / 20) + 1, 1);
             base.AddActionPreferencesToList(alreadyConsidered, log);
         }
 
         public override void ModifyVisDesires(Magus magus, VisDesire[] visDesires)
         {
-            visDesires[14].Quantity += (magus.SeasonalAge / 20) + 1;
-            visDesires[0].Quantity += (magus.SeasonalAge / 20) + 1;
+            foreach(Ability visAbility in visTypes)
+            {
+                visDesires[visAbility.AbilityId].Quantity += 7;
+            }
         }
     }
 }
